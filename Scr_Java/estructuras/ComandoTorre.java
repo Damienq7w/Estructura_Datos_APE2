a72094ae3963aa2e2d017ae9b56a63ad594263c1
tacurisantillanmonicasara-dot <tacurisantillanmonicasara@gmail.com>
2026-09-07 08:07:35 -0500

    Delete Scr_Java/estructuras/ComandoTorre.java

--- Scr_Java/estructuras/ComandoTorre.java
+++
@@ -1,8 +0,0 @@
-package estructuras;
-
-// Patron Command: encapsula una accion sobre una torre para poder
-// deshacerla/rehacerla despues, sin que HistorialComandos conozca los detalles.
-public interface ComandoTorre {
-    void ejecutar();
-    void deshacer();
-}
